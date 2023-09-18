0c4c809153f6bf5f7067440497c468739c660a2f
JBo123 <jakub.klucky05@gmail.com>
2023-09-18 08:07:49 +0200

    Initial Commit

---
+++ Notification Center/Notification_CenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Notification_CenterApp.swift
+//  Notification Center
+//
+//  Created by Jakub Klucký on 18.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Notification_CenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
