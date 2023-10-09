e8353736ebeb5bb5ceeb52defbeec6b5195ab1ab
JBo123 <jakub.klucky05@gmail.com>
2023-10-09 14:04:05 +0200

    some ui changes

--- Notification Center/Notifications/AddNotificationView.swift
+++ Notification Center/Notifications/AddNotificationView.swift
@@ -17,28 +17,41 @@ struct AddNotificationView: View {
     @State private var alertMessage = ""
     private var notification: Notification = Notification()
     var body: some View {
-        VStack {
-            TextField("Notification text", text: $notificationText)
+        NavigationView {
             
-            TextField("Seconds until notification", text: $secondsUntilNotification)
-                .onChange(of: secondsUntilNotification) { value in
-                    guard let number = Int(value),
-                          number > 0 else {
-                        self.secondsUntilNotification = ""
-                        return
-                    }
-                    self.secondsUntilNotification = String(number)
+            Form{
+                
+                Section(header: Text("Notification")) {
+                    TextField("Notification text", text: $notificationText)
+                    
+                    TextField("Seconds until notification", text: $secondsUntilNotification)
+                        .onChange(of: secondsUntilNotification) { value in
+                            guard let number = Int(value),
+                                  number > 0 else {
+                                self.secondsUntilNotification = ""
+                                return
+                            }
+                            self.secondsUntilNotification = String(number)
+                        }
                 }
+                Button("Start notification") {
+                    notification.addNotification(notificationText: notificationText,
+                                                 secondsUntilNotification: secondsUntilNotification,
+                                                 alertTitle: alertTitle, alertMessage: alertMessage,
+                                                 showAlert: showAlert)
+                    showAlert.toggle()
+                }
+                .alert(isPresented: $showAlert) {
+                    Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+                }
+                NavigationLink {
+                    ShowNotificationView()
+                } label: {
+                    Text("Show Notifications")
+                }
+
+            }.navigationTitle("Add Notification")
             
-            Button("Start notification") {
-                notification.addNotification(notificationText: notificationText,
-                                             secondsUntilNotification: secondsUntilNotification,
-                                             alertTitle: alertTitle, alertMessage: alertMessage,
-                                             showAlert: showAlert)
-            }
-            .alert(isPresented: $showAlert) {
-                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-            }
         }
     }
 }
---
+++ Notification Center/Notifications/ShowNotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  ShowNotificationView.swift
+//  Notification Center
+//
+//  Created by Jakub Klucký on 09.10.2023.
+//
+
+import SwiftUI
+
+struct ShowNotificationView: View {
+    var body: some View {
+        Text("Show Notification")
+    }
+}
+
+struct ShowNotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowNotificationView()
+    }
+}
